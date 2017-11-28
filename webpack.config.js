ba6fc4dc7ff90dc90002a402aa8d5e777cfae697
mizne <w20054319@126.com>
2017-11-28 22:57:36 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.ts',
@@ -21,7 +22,11 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
+  devtool: 'source-map',
   plugins: [
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['dist']),
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
   ]
 }
